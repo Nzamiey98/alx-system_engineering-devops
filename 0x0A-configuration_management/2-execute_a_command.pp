0729ff967935160388f54075e1a95cd08ca479e1
Nzamiey98 <124698373+Nzamiey98@users.noreply.github.com>
2023-11-13 13:55:26 +0200

    Delete 0x0A-configuration_management directory

--- 0x0A-configuration_management/2-execute_a_command.pp
+++
@@ -1,9 +0,0 @@
-# Kills a process named killmenow
-
-# Define the exec resource to kill the process
-exec { 'kill_killmenow_process':
-  command => 'pkill -f killmenow',
-  path    => '/usr/bin:/bin',
-  onlyif  => 'pgrep -f killmenow',
-}
-
