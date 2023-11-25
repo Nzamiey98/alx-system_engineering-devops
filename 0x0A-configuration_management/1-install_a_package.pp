a3e81bc36631f5eb260daad4a14c374d9a26269c
Nondzameko <nzamiey.com@gmail.com>
2023-11-25 08:57:22 -0800

    updated

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,13 @@
-#!/usr/bin/pup
-# install an especific version of flask (2.1.0)
-package {'flask':
-  ensure => '2.1.0',
-  provider => 'pip3'
+# Ensure pip3 is installed
+package { 'python3-pip':
+  ensure => installed,
+}
+
+# Install Flask version 2.1.0 using pip3
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+  creates => '/usr/local/lib/python3.*/dist-packages/Flask-2.1.0.dist-info',
+  require => Package['python3-pip'],
+  path    => '/usr/bin',
 }
 
